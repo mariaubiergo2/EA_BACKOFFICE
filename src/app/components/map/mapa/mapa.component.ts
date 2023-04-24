bc27ef3a3449721ac410833e9100d71b16c56a8a
marioRelajao <mario.cerda@estudiantat.upc.edu>
2023-04-24 20:07:03 +0200

    Added REAL map (need API KEY)

--- src/app/components/map/mapa/mapa.component.ts
+++ src/app/components/map/mapa/mapa.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-mapa',
   templateUrl: './mapa.component.html',
   styleUrls: ['./mapa.component.scss']
 })
-export class MapaComponent {
-
+export class MapaComponent implements OnInit {
+  constructor() {}
+  ngOnInit(): void {}
+  display: any;
+  center: google.maps.LatLngLiteral = {
+      lat: 41.2755222,
+      lng: 1.984643
+  };
+  zoom = 17;
+  moveMap(event: google.maps.MapMouseEvent) {
+      if (event.latLng != null) this.center = (event.latLng.toJSON());
+  }
+  move(event: google.maps.MapMouseEvent) {
+      if (event.latLng != null) this.display = event.latLng.toJSON();
+  }
 }
